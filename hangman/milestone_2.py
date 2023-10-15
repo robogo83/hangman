16968a8a5e0a8e52c78c85530d3351e1cf99b6f4
robert gore <gore.robert@gmail.com>
2023-10-15 10:54:55 +0100

    Adds milestone_2 file with initial logic

---
+++ hangman/milestone_2.py
@@ -0,0 +1,14 @@
+import random
+
+word_list = ['banana', 'apple', 'pear', 'orange', 'grapes']
+
+word = random.choice(word_list)
+
+guess = input("Please enter a single letter: ")
+
+if len(guess) == 1 and guess.isalpha():
+    print('Good guess!')
+else:
+    print('Oops that is not a valid input')
+
+print(word)
